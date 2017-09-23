428e566fab24cb64f7149f59654f170b0ca67a65
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-09-23 16:36:36 +0100

    As you wish

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,13 @@
 const messageInput = document.getElementById('messageInput')
 const sendBtn = document.getElementById('sendBtn')
 const chatLog = document.getElementById('chatLog')
+const userList = document.getElementById('userList')
+
+const addLi = (parent, innerHTML) => {
+  const li = document.createElement('li');
+  li.innerText = innerHTML;
+  parent.appendChild(li);
+}
 
 const socket = io();
 socket.emit('setUsername', userName)
@@ -13,4 +20,16 @@ socket.on('receiveMessage', function(message){
   const p = document.createElement('p')
   p.innerText = message
   chatLog.appendChild(p)
+})
+
+socket.on('updateUserList', function(allClients){
+  // Clean all users (li) from the user list (ul)
+  while (userList.firstChild) {
+    userList.removeChild(userList.firstChild);
+  }
+  
+  console.log('Amount of users connected: ' + Object.keys(allClients).length);
+  
+  // Render all usernames (li) again
+  allClients.map(username => addLi(userList, username))
 })
